2553231b49851a522ca965089ca006828a777530
Jack Sanders <jsanders4@sheffield.ac.uk>
2022-10-22 03:01:11 +0100

    Added support for separate training and testing data files

--- bayes.py
+++ bayes.py
@@ -5,14 +5,18 @@ import numpy as np
 parser = ArgumentParser()
 parser.add_argument("file", type=str, help="the name of your dataset")
 parser.add_argument("-m", "--method", type=str, choices=["onefile", "loo", "traintest"], default="onefile",
-                    help="bayesian classification method (onefile (default), loo, traintest (training/testing data in "
+                    help="bayesian classification method - onefile (default), loo, traintest (training/testing data in "
                          "separate files, appended with _train and _test, respectively)")
 
 args = parser.parse_args()
 
-dataset = args.file
-method = args.method
-X = np.loadtxt(open(f'data/{dataset}.txt', 'r'), delimiter=",")
+DATASET = args.file
+METHOD = args.method
+
+if METHOD == "traintest":
+    X = np.loadtxt(open(f'data/{DATASET}_train.txt', 'r'), delimiter=",")
+else:
+    X = np.loadtxt(open(f'data/{DATASET}.txt', 'r'), delimiter=",")
 
 
 def estimate_params(xs: np.array, n_factor: float = 0.1):
@@ -31,17 +35,27 @@ def split_data(xs: np.array, leave_out: int = False):
 
 # Find class labels and set up training and testing arrays
 labels = np.unique(X[:, -1])
+
 train_data = np.array([None for _ in labels])
-test_data = np.array([None for _ in labels])
 
-for i in range(len(labels)):
-    train_data[i], test_data[i] = split_data(X[X[:, -1] == labels[i]])
+if METHOD == "traintest":
+    for i in range(len(labels)):
+        train_data[i] = X[X[:, -1] == labels[i]]
 
-test_data = np.vstack(tuple(test_data))
+    test_data = np.loadtxt(open(f'data/{DATASET}_test.txt', 'r'), delimiter=",")
+
+else:
+    test_data = np.array([None for _ in labels])
+
+    for i in range(len(labels)):
+        train_data[i], test_data[i] = split_data(X[X[:, -1] == labels[i]])
+
+    test_data = np.vstack(tuple(test_data))
+
+## TODO - loo testing
 
 # Estimate priors - assume distribution within training data is accurate to real-world distribution
 priors = [len(t) / len(np.vstack(tuple(train_data))) for t in train_data]
-print(priors)
 
 params = [estimate_params(xs[:, :-1]) for xs in train_data]
 
@@ -52,4 +66,4 @@ normals = [multivariate_normal(mean=p[0], cov=p[1]) for p in params]
 posteriors = np.array([n.pdf(test_data[:, :-1]) for n in normals]).T * priors
 
 percent_right = np.count_nonzero(np.argmax(posteriors, axis=1) + 1 == test_data[:, -1]) / len(test_data) * 100
-print(f'\nClassified {round(percent_right, 2)}% of the training data correctly (2D.P.)')
+print(f'\nClassified {round(percent_right, 2)}% of the testing data correctly (2D.P.)')
