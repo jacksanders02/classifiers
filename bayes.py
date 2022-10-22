54071ad0366faa48d52144fffe2b72d544ef8ad8
Jack Sanders <jsanders4@sheffield.ac.uk>
2022-10-22 15:45:38 +0100

    Implemented leave one out training

--- bayes.py
+++ bayes.py
@@ -34,11 +34,30 @@ def estimate_params(xs: np.array, n_factor: float = 0.1):
 
 
 # split data into training and testing sets
-def split_data(xs: np.array, leave_out: int = False):
-    if leave_out:
-        return np.delete(xs, leave_out), xs[leave_out]
+def split_data(xs: np.array, leave_out: int = None):
+    if leave_out is not None:
+        return np.delete(xs, leave_out, axis=0), xs[leave_out]
     else:
         return xs[0::2], xs[1::2]
+
+
+def bayesian_classify(train, test):
+    # Estimate priors - assume distribution within training data is accurate to real-world distribution
+    priors = [len(t) / len(np.vstack(tuple(train))) for t in train]
+
+    params = [estimate_params(xs[:, :-1]) for xs in train]
+
+    # Construct gaussian distributions using estimated parameters
+    normals = [multivariate_normal(mean=p[0], cov=p[1]) for p in params]
+
+    tst = test[:, :-1] if len(test.shape) > 1 else test[:-1]
+
+    # Bayes classification rule - posterior probability {P(w|x)} = likelihood {P(x|w)} * prior
+    posteriors = np.array([dist.pdf(tst) for dist in normals]).T * priors
+
+    correct = test[:, -1] if len(test.shape) > 1 else test[-1]
+
+    return np.argmax(posteriors, axis=len(test.shape) - 1) + 1 == correct
 
 
 # Load dataset
@@ -55,6 +74,9 @@ labels = np.unique(X[:, -1])
 
 train_data = np.array([None for _ in labels])
 
+percent_right = 0
+
+# Training and Testing data in separate files
 if METHOD == "traintest":
     for i in range(len(labels)):
         train_data[i] = X[X[:, -1] == labels[i]]
@@ -62,7 +84,21 @@ if METHOD == "traintest":
     start = time.time()
     test_data = loadfile(DATASET, "_test")
     print(f'\nLoaded testing data in {round(time.time() - start, 2)} seconds')
+    percent_right = np.count_nonzero(bayesian_classify(train_data, test_data)) / len(test_data) * 100
 
+# Leave-One-Out training method
+elif METHOD == "loo":
+    n = 0
+    for i in range(X.shape[0]):
+        left_out, test_data = split_data(X, i)
+
+        for j in range(len(labels)):
+            train_data[j] = left_out[left_out[:, -1] == labels[j]]
+        n += bayesian_classify(train_data, test_data)
+
+    percent_right = n / X.shape[0] * 100
+
+# Split one file in half, one half is training data, and the other is testing
 else:
     test_data = np.array([None for _ in labels])
 
@@ -70,19 +106,6 @@ else:
         train_data[i], test_data[i] = split_data(X[X[:, -1] == labels[i]])
 
     test_data = np.vstack(tuple(test_data))
+    percent_right = np.count_nonzero(bayesian_classify(train_data, test_data)) / len(test_data) * 100
 
-## TODO - loo testing
-
-# Estimate priors - assume distribution within training data is accurate to real-world distribution
-priors = [len(t) / len(np.vstack(tuple(train_data))) for t in train_data]
-
-params = [estimate_params(xs[:, :-1]) for xs in train_data]
-
-# Construct gaussian distributions using estimated parameters
-normals = [multivariate_normal(mean=p[0], cov=p[1]) for p in params]
-
-# Bayes classification rule - posterior probability {P(w|x)} = likelihood {P(x|w)} * prior
-posteriors = np.array([n.pdf(test_data[:, :-1]) for n in normals]).T * priors
-
-percent_right = np.count_nonzero(np.argmax(posteriors, axis=1) + 1 == test_data[:, -1]) / len(test_data) * 100
-print(f'\nClassified {round(percent_right, 2)}% of the testing data correctly (2D.P.)')
+print(f'\nClassified {round(percent_right, 2)}% of the testing data correctly with {METHOD} method. (To 2 D.P.)')
